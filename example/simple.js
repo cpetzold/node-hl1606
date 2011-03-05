a6b85175f3ace5ac596c3231486b94fdd98693fe
Conner <cpetzold@gmail.com>
2011-03-05 03:14:13 -0800

    simplifying example

--- example/simple.js
+++ example/simple.js
@@ -2,23 +2,7 @@ var HL1606 = require('../lib/hl1606');
 
 var strip = HL1606.connect('/dev/ttyACM0', 115200);
 
-function nextColor(c) {
-  switch (c) {
-    case strip.Command | strip.RedOn: return strip.Command | strip.GreenOn;
-    case strip.Command | strip.GreenOn: return strip.Command | strip.BlueOn;
-    case strip.Command | strip.BlueOn: return strip.Command | strip.RedOn;
-    default: return strip.Command | strip.RedOn;
-  }
-}
-
-var n = 0;
-var c = nextColor();
-while(true) {
-  strip.push(c);
-  if (n == 120) {
-    n = 0;
-    c = nextColor(c);
-  }
-  n++;
-  strip.delay(250);
-}
+setInterval(function(){
+  strip.push(strip.Command | strip.GreenOn);
+  //strip.delay(250);
+}, 1000);
